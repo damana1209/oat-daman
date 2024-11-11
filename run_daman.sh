af6399c998cee408c6bfa556960b89437430cd19
Daman Arora <damana@andrew.cmu.edu>
2024-11-10 23:08:00 -0600

    bash runner

---
+++ run_daman.sh
@@ -0,0 +1,23 @@
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/u/darora1/miniconda3/envs/oat/lib/"
+
+python -m oat.experiment.main \
+    --gpus 2 \
+    --collocate \
+    --dap-algo DPO \
+    --beta 2 \
+    --reward-oracle pairrm \
+    --pretrain trl-lib/pythia-1b-deduped-tldr-sft \
+    --prompt-data lkevinzc/tldr-with-sft-reference \
+    --output_key pythia-1b-reference \
+    --sync-params-every 1 \
+    --rollout-batch-size-per-device 1 \
+    --pi-buffer-maxlen-per-device 64 \
+    --train-batch-size-per-device 8 \
+    --num-samples 10 \
+    --max-eval 10 \
+    --use-wb \
+    --max-train 10 \
+    --wb-run-name 1b_pairrm_dpo_online
+
+
+# rollout-batch-size-per-device should be 64
